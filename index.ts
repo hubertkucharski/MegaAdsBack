99e735e02396fde7ffde8265ad3d7c944b6e267b
hubertkucharski <hubertkucharski@gmail.com>
2022-05-14 12:01:37 +0200

    Added errors handling.

--- index.ts
+++ index.ts
@@ -1,6 +1,8 @@
 import express, {json} from 'express';
 import cors from 'cors';
 import 'express-async-errors';
+import {handleError, ValidationError} from "./utils/errors";
+// import('./utils/db');
 
 const app = express();
 
@@ -9,6 +11,17 @@ app.use(cors({
 }));
 app.use(json());
 
+app.use((req, res, next) => {
+    console.log('time: ', new Date().toLocaleTimeString() )
+    next()
+})
+
+// app.get('/', async (req, res) => {
+//     throw new ValidationError('Hola hola!')
+// })
+
+
+app.use(handleError)
 
 app.listen(3001, '0.0.0.0', ()=>{
     console.log('Running server on http://localhost:3001')
